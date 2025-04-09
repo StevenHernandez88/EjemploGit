4dd8738758cddc0477c02cac33f489eab7fb0afa
StevenHernandez88 <mcluyersteven@gmail.com>
2025-04-09 10:42:06 -0500

    Primer Commit

---
+++ Carro.java
@@ -0,0 +1,14 @@
+public class Carro{
+    String color;
+    String placa;
+
+    public Carro(String color, String placa){
+        this.color = color;
+        this.placa = placa;
+    }
+
+    public String toString(color, placa){
+        System.out.print("color: "+ color + "placa: "+placa);
+    }
+
+}
